9680191024ffdaae4e3bcf6a01f44f2b4ac6f2fd
larsvevo <ardaadinc2003@gmail.com>
2024-11-23 12:13:32 +0300

    import carbon

--- src/Http/Controllers/HomeController.php
+++ src/Http/Controllers/HomeController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\DB;
 use Illuminate\View\View;
-
+use Carbon\Carbon;
 class HomeController extends Controller
 {
     /**
